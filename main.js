12cd93aaebda51f114a2549c033175cf3e1c3f99
gingu-code <48529532+gingu-code@users.noreply.github.com>
2020-09-16 12:14:20 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,38 @@
+//loop through the object using forEach so that it logs, (eg,for user1)
+//'user1 is called Jon and is 23 years old'
+//the loop should not return a data structure
+// Use one or more of the object methods to solve
+const obj = {
+  user1: {
+    name: 'Jon',
+    age: 23,
+  },
+  user2: {
+    name: 'Jen',
+    age: 25,
+  },
+  user3: {
+    name: 'Jan',
+    age: 21,
+  },
+};
+// Use Object Methods
+//Create an albphabeticl list of every folder in the documents object (the keys are the folders)
+//Create a list of every file in the documents object. one list.
+const documents = {
+  taxes: ['2010.xlx', '2020.xlx'],
+  records: ['rec1.docx', 'rec2.docx', 'rec3.docx'],
+  miscellaneous: ['pic.jpg', 'passwords.pdf', 'docs.txt'],
+  Clubhouse: ['paty.jpg', 'christmasRules.pdf', 'pool.txt'],
+};
+//Are the two books alike? Create a function and Use Object methods to find out. Change object values to test for non-alike objects
+const book1 = {
+  title: 'My Book',
+  author: 'John Doe',
+  year: '2019',
+};
+const book2 = {
+  title: 'My Book',
+  author: 'John Doe',
+  year: '2019',
+};
